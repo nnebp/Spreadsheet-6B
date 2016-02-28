558b5cc387ae0b1df044ea9d7c449039c98d4764
munkhuush92 <king23_gmb@yahoo.com>
2016-02-28 11:46:57 -0800

    Donald Chinn's starter code methods are added

--- Cell.java
+++ Cell.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Cell {
  private String formula;
  private int value;
@@ -43,6 +44,368 @@ public class Cell {
         }
         return sb.reverse().toString();
     }
+    
+    
+    /**
+ * getCellToken
+ * 
+ * Assuming that the next chars in a String (at the given startIndex)
+ * is a cell reference, set cellToken's column and row to the
+ * cell's column and row.
+ * If the cell reference is invalid, the row and column of the return CellToken
+ * are both set to BadCell (which should be a final int that equals -1).
+ * Also, return the index of the position in the string after processing
+ * the cell reference.
+ * (Possible improvement: instead of returning a CellToken with row and
+ * column equal to BadCell, throw an exception that indicates a parsing error.)
+ * 
+ * A cell reference is defined to be a sequence of CAPITAL letters,
+ * followed by a sequence of digits (0-9).  The letters refer to
+ * columns as follows: A = 0, B = 1, C = 2, ..., Z = 25, AA = 26,
+ * AB = 27, ..., AZ = 51, BA = 52, ..., ZA = 676, ..., ZZ = 701,
+ * AAA = 702.  The digits represent the row number.
+ *
+ * @param inputString  the input string
+ * @param startIndex  the index of the first char to process
+ * @param cellToken  a cellToken (essentially a return value)
+ * @return  index corresponding to the position in the string just after the cell reference
+ */
+public static int getCellToken (String inputString, int startIndex, CellToken cellToken) {
+    char ch;
+    int column = 0;
+    int row = 0;
+    int index = startIndex;
+
+    // handle a bad startIndex
+    if ((startIndex < 0) || (startIndex >= inputString.length() )) {
+       throw new IllegalArgumentException(" Parsing error ");
+        
+    }
+
+    // get rid of leading whitespace characters
+    while (index < inputString.length() ) {
+        ch = inputString.charAt(index);            
+        if (!Character.isWhitespace(ch)) {
+            break;
+        }
+        index++;
+    }
+    if (index == inputString.length()) {
+        // reached the end of the string before finding a capital letter
+        throw new IllegalArgumentException("Empty cell error");
+        
+    }
+
+    // ASSERT: index now points to the first non-whitespace character
+
+    ch = inputString.charAt(index);            
+    // process CAPITAL alphabetic characters to calculate the column
+    if (!Character.isUpperCase(ch)) {
+        
+       throw new IllegalArgumentException("Non-capitalized letters are not allowed");
+    } else {
+        column = ch - 'A';
+        index++;
+    }
+
+    while (index < inputString.length() ) {
+        ch = inputString.charAt(index);            
+        if (Character.isUpperCase(ch)) {
+            column = ((column + 1) * 26) + (ch - 'A');
+            index++;
+        } else {
+            break;
+        }
+    }
+    if (index == inputString.length() ) {
+        // reached the end of the string before fully parsing the cell reference
+        throw new IllegalArgumentException("NOt a complete cell");
+    }
+
+    // ASSERT: We have processed leading whitespace and the
+    // capital letters of the cell reference
+
+    // read numeric characters to calculate the row
+    if (Character.isDigit(ch)) {
+        row = ch - '0';
+        index++;
+    } else {
+        throw new IllegalArgumentException("Non-numerical data");
+    }
+
+    while (index < inputString.length() ) {
+        ch = inputString.charAt(index);            
+        if (Character.isDigit(ch)) {
+            row = (row * 10) + (ch - '0');
+            index++;
+        } else {
+            break;
+        }
+    }
+
+    // successfully parsed a cell reference
+    cellToken.setColumn(column);
+    cellToken.setRow(row);
+    return index;
+}
+
+public static void main(String [] args)
+	{
+	    CellToken token = new CellToken();
+	   System.out.println(getCellToken("AA27 + 6", 0, token));
+	   
+	   System.out.println(token.getRow());
+	      System.out.println(token.getCol());
+	      Cell newClass = new Cell();
+	      
+	      System.out.println(newClass.printCellToken(token));
+	   }
+
+	   
+	   /**
+ * getFormula
+ * 
+ * Given a string that represents a formula that is an infix
+ * expression, return a stack of Tokens so that the expression,
+ * when read from the bottom of the stack to the top of the stack,
+ * is a postfix expression.
+ * 
+ * A formula is defined as a sequence of tokens that represents
+ * a legal infix expression.
+ * 
+ * A token can consist of a numeric literal, a cell reference, or an
+ * operator (+, -, *, /).
+ * 
+ * Multiplication (*) and division (/) have higher precedence than
+ * addition (+) and subtraction (-).  Among operations within the same
+ * level of precedence, grouping is from left to right.
+ * 
+ * This algorithm follows the algorithm described in Weiss, pages 105-108.
+ */
+public Stack getFormula(String formula) {
+    Stack returnStack = new Stack();  // stack of Tokens (representing a postfix expression)
+    boolean error = false;
+    char ch = ' ';
+
+    int literalValue = 0;
+
+    CellToken cellToken;
+    int column = 0;
+    int row = 0;
+
+    int index = 0;  // index into formula
+    Stack operatorStack = new Stack();  // stack of operators
+
+    while (index < formula.length() ) {
+        // get rid of leading whitespace characters
+        while (index < formula.length() ) {
+            ch = formula.charAt(index);
+            if (!Character.isWhitespace(ch)) {
+                break;
+            }
+            index++;
+        }
+
+        if (index == formula.length() ) {
+            error = true;
+            break;
+        }
+
+        // ASSERT: ch now contains the first character of the next token.
+        if (isOperator(ch)) {
+            // We found an operator token
+            switch (ch) {
+                case OperatorToken.Plus:
+                case OperatorToken.Minus:
+                case OperatorToken.Mult:
+                case OperatorToken.Div:
+                case OperatorToken.LeftParen:
+                    // push operatorTokens onto the output stack until
+                    // we reach an operator on the operator stack that has
+                    // lower priority than the current one.
+                    OperatorToken stackOperator;
+                    while (!operatorStack.isEmpty()) {
+                        stackOperator = (OperatorToken) operatorStack.pop();
+                        if ( (stackOperator.priority() >= operatorPriority(ch)) &&
+                            (stackOperator.getOperatorToken() != OperatorToken.LeftParen) ) {
+
+                            // output the operator to the return stack    
+                            operatorStack.pop();
+                            returnStack.push(stackOperator);
+                        } else {
+                            break;
+                        }
+                    }
+                    break;
+
+                default:
+                    // This case should NEVER happen
+                    System.out.println("Error in getFormula.");
+                    System.exit(0);
+                    break;
+            }
+            // push the operator on the operator stack
+            operatorStack.push(new OperatorToken(ch));
+
+            index++;
+
+        } else if (ch == ')') {    // maybe define OperatorToken.RightParen ?
+            OperatorToken stackOperator;
+            stackOperator = (OperatorToken) operatorStack.pop();
+            // This code does not handle operatorStack underflow.
+            while (stackOperator.getOperatorToken() != OperatorToken.LeftParen) {
+                // pop operators off the stack until a LeftParen appears and
+                // place the operators on the output stack
+                returnStack.push(stackOperator);
+                stackOperator = (OperatorToken) operatorStack.pop();
+            }
+
+            index++;
+        } else if (Character.isDigit(ch)) {
+            // We found a literal token
+            literalValue = ch - '0';
+            index++;
+            while (index < formula.length()) {
+                ch = formula.charAt(index);
+                if (Character.isDigit(ch)) {
+                    literalValue = (literalValue * 10) + (ch - '0');
+                    index++;
+                } else {
+                    break;
+                }
+            }
+            // place the literal on the output stack
+            returnStack.push(new LiteralToken(literalValue));
+
+        } else if (Character.isUpperCase(ch)) {
+            // We found a cell reference token
+            CellToken cellTokenref = new CellToken();
+            index = getCellToken(formula, index, cellTokenref);
+            if (cellTokenref.getRow() <= 0) {
+                error = true;
+                break;
+            } else {
+                // place the cell reference on the output stack
+                returnStack.push(cellTokenref);
+            }
+
+        } else {
+            error = true;
+            break;
+        }
+    }
+
+    // pop all remaining operators off the operator stack
+    while (!operatorStack.isEmpty()) {
+        returnStack.push(operatorStack.pop());
+    }
+
+    if (error) {
+        // a parse error; return the empty stack
+       throw new IllegalArgumentException("Bad cell token inputs");
+    }
+
+    return returnStack;
+}
+
+/**
+ * Return true if the char ch is an operator of a formula.
+ * Current operators are: +, -, *, /, (.
+ * @param ch  a char
+ * @return  whether ch is an operator
+ */
+public boolean isOperator (char ch) {
+    return ((ch == '+') ||
+            (ch == '-') ||
+            (ch == '*') ||
+            (ch == '/') ||
+            (ch == '(') );
+}
+
+
+
+/**
+ * Given an operator, return its priority.
+ *
+ * priorities:
+ *   +, - : 0
+ *   *, / : 1
+ *   (    : 2
+ *
+ * @param ch  a char
+ * @return  the priority of the operator
+ */
+public int operatorPriority (char ch) {
+    if (!isOperator(ch)) {
+        // This case should NEVER happen
+        System.out.println("Error in operatorPriority.");
+        System.exit(0);
+    }
+    switch (ch) {
+        case '+':
+            return 0;
+        case '-':
+            return 0;
+        case '*':
+            return 1;
+        case '/':
+            return 1;
+        case '(':
+            return 2;
+
+        default:
+            // This case should NEVER happen
+            System.out.println("Error in operatorPriority.");
+            System.exit(0);
+            break;
+    }
+    return -1;
+}
+
+
+
+/**
+ *  Given a CellToken, print it out as it appears on the
+ *  spreadsheet (e.g., "A3")
+ *  @param cellToken  a CellToken
+ *  @return  the cellToken's coordinates
+ */
+public String printCellToken (CellToken cellToken) {
+    char ch;
+    String returnString = "";
+    int col;
+    int largest = 26;  // minimum col number with number_of_digits digits
+    int number_of_digits = 2;
+
+    col = cellToken.getCol();
+
+    // compute the biggest power of 26 that is less than or equal to col
+    // We don't check for overflow of largest here.
+    while (largest <= col) {
+        largest = largest * 26;
+        number_of_digits++;
+    }
+    largest = largest / 26;
+    number_of_digits--;
+
+    // append the column label, one character at a time
+    while (number_of_digits > 1) {
+        ch = (char) (((col / largest) - 1) + 'A');
+        returnString += ch;
+        col = col % largest;
+        largest = largest  / 26;
+        number_of_digits--;
+    }
+
+    // handle last digit
+    ch = (char)(col + 'A');
+    returnString += ch;
+
+    // append the row as an integer
+    returnString += cellToken.getRow();
+
+    return returnString;
+}
 
 
 }
