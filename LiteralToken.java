f1fb4c59a6b04344e16d1f6928681b93929792f2
sheera89 <sheera89@uw.edu>
2016-03-06 17:37:52 -0800

    Added files via upload

--- LiteralToken.java
+++ LiteralToken.java
@@ -1,17 +1,37 @@
-public class LiteralToken extends Token {
- private int myLiteral;
+/**
+ * Token class which represents a literal integer 
+ * term in a postfix expression tree.
+ */
  
- public LiteralToken(int value){
-    
-    myLiteral = value;
+public class LiteralToken extends Token {
+	private int myLiteral;
+	
+	/**
+	 * Default constructor. Takes the input and makes 
+	 * it the int value of this Literal token.
+	 * 
+	 * @param value the value of the Literal token
+	 */
+	public LiteralToken(int value){
+		myLiteral = value;
     }
     
-    
+    /**
+	 * Returns the value of this Literal token.
+	 *
+	 * @return value of this Literal token
+	 */
     public int getLiteral() {
         return myLiteral;
     
     }
     
+	/**
+	 * Returns a string representation of the integer 
+	 * value of this Literal token.
+	 * @return String containing the integer value of this 
+	 * Literal token
+	 */
     public String toString() {
     	return new Integer(myLiteral).toString();
     }
